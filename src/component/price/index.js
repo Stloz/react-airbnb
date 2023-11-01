f18d50519ede0e9405ae3c6636f40cdf95a959a0
Stloz <stloz.serg@gmail.com>
2023-11-01 23:54:23 +0200

    practice part 1

---
+++ src/component/price/index.js
@@ -0,0 +1,57 @@
+import "./index.css";
+
+import Box from "../box";
+
+export default function Price({ price, discount, currency, ...rest }) {
+  return (
+    <Box className="price">
+      <div class="price__header">
+        <span
+          className={`price__value ${
+            discount ? "price__value--has--discount" : ""
+          }`}
+        >
+          {currency}
+          {price}
+        </span>
+        {discount && (
+          <span className={`price__value`}>
+            {currency}
+            {discount}
+          </span>
+        )}
+      </div>
+
+      <List {...rest} currency={currency} />
+    </Box>
+  );
+}
+
+function List({ currency, cleaning, service, checkin, checkout }) {
+  return (
+    <ul className="price__list">
+      <li className="price__list-item">
+        <span>Плата за прибирання:</span>
+        <span>
+          {currency}
+          {cleaning}
+        </span>
+      </li>
+      <li className="price__list-item">
+        <span>Сервісний збір:</span>
+        <span>
+          {currency}
+          {service}
+        </span>
+      </li>
+      <li className="price__list-item">
+        <span>Дата прибуття:</span>
+        <span>{checkin}</span>
+      </li>
+      <li className="price__list-item">
+        <span>Дата відʼїзду:</span>
+        <span>{checkout}</span>
+      </li>
+    </ul>
+  );
+}
